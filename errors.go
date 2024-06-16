0bafff8ec0b1e4108c75b036ba9886daef7e618c
Åsulv Vaa <aasulvv@stud.ntnu.no>
2024-06-16 17:46:17 +0200

    Added new rate limiting errors, invalid credentials error and invalid data error

--- errors.go
+++ errors.go
@@ -8,10 +8,14 @@ import (
 
 // Error IDs
 const ERRID_RATE_LIMIT = "RATE_LIMITED"
+const ERRID_RL_COOLDOWN = "RATE_LIMITED_COOLDOWN"
+const ERRID_RL_LOGIN = "RATE_LIMITED_LOGIN"
 const ERRID_PROCESSING = "PROCESSING_ERROR"
 const ERRID_DECODING = "JSON_DECODING_ERROR"
 const ERRID_UNAUTHORIZED = "UNAUTHORIZED"
 const ERRID_UNSUPPORTED_METHOD = "UNSUPPORTED_METHOD"
+const ERRID_INVALID_CREDENTIALS = "INVALID_CREDENTIALS"
+const ERRID_INVALID_DATA = "INVALID_DATA"
 
 // Error types
 func RateLimitError(ip string) *VASError {
@@ -23,6 +27,29 @@ func RateLimitError(ip string) *VASError {
 		GoError:          errors.New(fmt.Sprintf("ip %s sending too many requests", ip)),
 	}
 	return &err
+}
+
+func RateLimitCooldownError(ip string, cooldown uint, limiterName string) *VASError {
+	desc := fmt.Sprintf("Please wait %d seconds before trying again", cooldown)
+	err := VASError{
+		ErrorId:          ERRID_RL_COOLDOWN,
+		ErrorName:        "You have been sending too many requests!",
+		ErrorDescription: &desc,
+		StatusCode:       http.StatusTooManyRequests,
+		GoError:          errors.New(fmt.Sprintf("ip %s sending too many requests, limited by %s for %d second(s)", ip, limiterName, cooldown)),
+	}
+	return &err
+}
+
+func RateLimitLoginError(ip string, userId uint, cooldown uint) *VASError {
+	desc := fmt.Sprintf("Please wait %d seconds before trying again", cooldown)
+	return &VASError{
+		ErrorId:          ERRID_RL_LOGIN,
+		ErrorName:        "You have been sending too many login requests!",
+		ErrorDescription: &desc,
+		StatusCode:       http.StatusTooManyRequests,
+		GoError:          errors.New(fmt.Sprintf("too many login requests for user with ID %d coming from %s", userId, ip)),
+	}
 }
 
 func ProcessingError(errType string, err error) *VASError {
@@ -73,5 +100,27 @@ func UnsupportedMethodError(methodUsed string, allowedMethods []string) *VASErro
 		ErrorDescription: &desc,
 		StatusCode:       http.StatusMethodNotAllowed,
 		GoError:          errors.New(fmt.Sprintf("client used an unsupported method, %s", methodUsed)),
+	}
+}
+
+func InvalidCredentialsError(err error) *VASError {
+	desc := "Wrong username/password combination"
+	return &VASError{
+		ErrorId:          ERRID_INVALID_CREDENTIALS,
+		ErrorName:        "Invalid credentials",
+		ErrorDescription: &desc,
+		StatusCode:       http.StatusUnauthorized,
+		GoError:          err,
+	}
+}
+
+func InvalidDataError(field string, err error) *VASError {
+	desc := fmt.Sprintf("Applies to field '%s'", field)
+	return &VASError{
+		ErrorId:          ERRID_INVALID_DATA,
+		ErrorName:        "Invalid input data found while processing your request",
+		ErrorDescription: &desc,
+		StatusCode:       http.StatusNotAcceptable,
+		GoError:          nil,
 	}
 }
